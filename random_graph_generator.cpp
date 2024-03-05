70a0fbdfe68c9bbeafe040870d81d5f37bd26b4e
truecuozzo98 <antonio.pugliese9898@gmail.com>
2024-03-05 17:05:49 +0100

    initial commit

---
+++ random_graph_generator.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define MAX_EDGE_WEIGHT 100
+#define MAX_NODES 100000000
+#define MIN_EDGE_WEIGHT 10
+
+using namespace std;
+
+void generate(int nodes) {
+vector<vector<pair<long long int, long long int> > > adjacency(nodes);
+
+    long long int extra_edges = ((nodes - 1) * nodes)/2 - (nodes-1);
+    extra_edges = rand() % (extra_edges + 1);
+
+    if(nodes - 1 + extra_edges > MAX_NODES){
+        long long int difference = MAX_NODES - (nodes - 1);
+        extra_edges = rand() % (difference + 1);
+    }
+
+    vector<long long int> graph(nodes);
+    
+    for(long long int i = 0; i < nodes; ++i){
+        graph[i] = i;
+    }   
+
+    random_shuffle(graph.begin(),graph.end());
+
+    set<pair<long long int, long long int> > present_edge;
+
+    for(long long int i = 1; i < nodes; ++i){
+        long long int add = rand() % i;
+        long long int weight = rand() % MAX_EDGE_WEIGHT + MIN_EDGE_WEIGHT;
+        adjacency[graph[i]].push_back(make_pair(graph[add], weight));
+        adjacency[graph[add]].push_back(make_pair(graph[i], weight));
+        present_edge.insert(make_pair(min(graph[add], graph[i]), max(graph[add], graph[i])));
+    }
+
+    for(long long int i = 1; i <= extra_edges; ++i){
+        long long int weight = rand() % MAX_EDGE_WEIGHT + MIN_EDGE_WEIGHT;
+        while(1){
+            long long int node1 = rand() % nodes;
+            long long int node2 = rand() % nodes;
+            if(node1 == node2) continue;
+            if(present_edge.find(make_pair(min(node1, node2), max(node1, node2))) == present_edge.end()){
+                adjacency[node1].push_back(make_pair(node2, weight));
+                adjacency[node2].push_back(make_pair(node1, weight));
+                present_edge.insert(make_pair(min(node1, node2), max(node1, node2)));
+                break;
+            }
+        }
+    }
+
+    //char file_name[50];
+    //snprintf(file_name, sizeof(file_name), "generated_graph_%d", nodes);
+
+    freopen("graph", "w", stdout);
+    
+    cout<<nodes<<" "<<nodes-1+extra_edges<<"\n";
+
+    for(long long int i = 0; i < nodes; ++i){
+        for(long long int j = 0; j < adjacency[i].size(); ++j){
+            if(i < adjacency[i][j].first){
+                cout<<i<<" "<<adjacency[i][j].first<<" "<<adjacency[i][j].second<<"\n";
+            }
+        }
+    }
+
+    //fclose(stdout);
+}
+
+int main(){
+
+    srand(time(NULL));
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    // nodes<=MAX_NODES
+
+    printf("Insert the number of nodes: ");
+
+    long long int nodes;
+    cin>>nodes;
+
+    generate(nodes);
+    //generate(20);
+    //generate(30);
+    
+
+    return 0;
+}
